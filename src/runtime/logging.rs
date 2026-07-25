faf58dd1acd2388e8d3758d09cced572b983d282
Spartaco Amadei <spartaco.amadei@outlook.it>
2026-07-26 01:53:58 +0200

    refactor: add `UtcDateTime` for UTC logging and update macros to use `timestamp_utc`

--- src/runtime/logging.rs
+++ src/runtime/logging.rs
@@ -33,6 +33,88 @@ mod private {
 
         unsafe { ts.QuadPart }
     }
+    const WINDOWS_TO_UNIX_EPOCH_100NS: i64 = 116_444_736_000_000_000;
+    const HUNDRED_NS_PER_SEC: i64 = 10_000_000;
+    const HUNDRED_NS_PER_MILLI: i64 = 10_000;
+    const SECS_PER_DAY: i64 = 86_400;
+
+    #[derive(Copy, Clone, Debug)]
+    pub struct UtcDateTime {
+        pub year: i32,
+        pub month: u8,
+        pub day: u8,
+        pub hour: u8,
+        pub minute: u8,
+        pub second: u8,
+        pub millis: u16,
+    }
+
+    impl core::fmt::Display for UtcDateTime {
+        fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+            write!(
+                f,
+                "{:04}-{:02}-{:02} {:02}:{:02}:{:02}.{:03} UTC",
+                self.year,
+                self.month,
+                self.day,
+                self.hour,
+                self.minute,
+                self.second,
+                self.millis
+            )
+        }
+    }
+
+    #[inline]
+    pub fn filetime_100ns_to_unix_parts(ts100ns: i64) -> (i64, u16) {
+        let unix_100ns = ts100ns - WINDOWS_TO_UNIX_EPOCH_100NS;
+        let secs = unix_100ns / HUNDRED_NS_PER_SEC;
+        let millis = ((unix_100ns % HUNDRED_NS_PER_SEC) / HUNDRED_NS_PER_MILLI) as u16;
+        (secs, millis)
+    }
+
+    #[inline]
+    fn civil_from_days(days_since_unix_epoch: i64) -> (i32, u8, u8) {
+        let z = days_since_unix_epoch + 719_468;
+        let era = if z >= 0 { z } else { z - 146_096 } / 146_097;
+        let doe = z - era * 146_097;
+        let yoe = (doe - doe / 1460 + doe / 36_524 - doe / 146_096) / 365;
+        let y = yoe + era * 400;
+        let doy = doe - (365 * yoe + yoe / 4 - yoe / 100);
+        let mp = (5 * doy + 2) / 153;
+        let d = doy - (153 * mp + 2) / 5 + 1;
+        let m = mp + if mp < 10 { 3 } else { -9 };
+        let year = y + if m <= 2 { 1 } else { 0 };
+
+        (year as i32, m as u8, d as u8)
+    }
+
+    #[inline]
+    pub fn unix_secs_to_utc(secs: i64, millis: u16) -> UtcDateTime {
+        let days = secs.div_euclid(SECS_PER_DAY);
+        let sod = secs.rem_euclid(SECS_PER_DAY);
+
+        let (year, month, day) = civil_from_days(days);
+        let hour = (sod / 3600) as u8;
+        let minute = ((sod % 3600) / 60) as u8;
+        let second = (sod % 60) as u8;
+
+        UtcDateTime {
+            year,
+            month,
+            day,
+            hour,
+            minute,
+            second,
+            millis,
+        }
+    }
+
+    #[inline]
+    pub fn timestamp_utc() -> UtcDateTime {
+        let (secs, millis) = filetime_100ns_to_unix_parts(timestamp());
+        unix_secs_to_utc(secs, millis)
+    }
 
     #[must_use]
     #[allow(dead_code)]
@@ -65,10 +147,10 @@ macro_rules! error {
         swdk::println!(
             "{}[{}][error]: {} in {}:{} at line {}",
             $crate::logger_name!(),
-            $crate::rt::logging::timestamp(),
+            ::swdk::rt::logging::timestamp_utc(),
             format_args!($($arg)*),
-            $crate::rt::logging::remove_module_name(core::module_path!()),
-            $crate::rt::logging::file_name(core::file!()),
+            ::swdk::rt::logging::remove_module_name(core::module_path!()),
+            ::swdk::rt::logging::file_name(core::file!()),
             core::line!()
         );
     }};
@@ -80,7 +162,7 @@ macro_rules! info {
         swdk::println!(
             "{}[{}][info]: {}",
             $crate::logger_name!(),
-            $crate::rt::logging::timestamp(),
+            $crate::rt::logging::timestamp_utc(),
             format_args!($($arg)*)
         );
     }};
@@ -93,7 +175,7 @@ macro_rules! debug {
         swdk::println!(
             "{}[{}][debug]: {}",
             $crate::logger_name!(),
-            $crate::rt::logging::timestamp(),
+            ::swdk::rt::logging::timestamp_utc(),
             format_args!($($arg)*),
         );
     }};
@@ -111,10 +193,10 @@ macro_rules! warn {
         wdk::println!(
             "{}[{}][warn]: {} in {}:{}",
             $crate::logger_name!(),
-            $crate::rt::logging::timestamp(),
+            ::swdk::rt::logging::timestamp_utc(),
             alloc::format!($($arg)*),
-            $crate::rt::logging::remove_module_name(core::module_path!()),
-            $crate::rt::logging::file_name(core::file!()),
+            ::swdk::rt::logging::remove_module_name(core::module_path!()),
+            ::swdk::rt::logging::file_name(core::file!()),
         );
     }};
 }
