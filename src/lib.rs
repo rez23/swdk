42e483cf6ddb50b4eda664143bc51946111d786d
Spartaco Amadei <spartaco.amadei@outlook.it>
2026-07-28 02:53:43 +0200

    style: clean up imports and improve consistency in `swdk/src/lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -10,16 +10,21 @@
         impl_trait_in_assoc_type,
     )
 )]*/
-#[cfg(all(not(feature = "test-runtime"), not(feature = "kmdf-runtime")))]
+#[cfg(all(
+    not(feature = "test-runtime"),
+    not(feature = "kmdf-runtime")
+))]
 compile_error!(
     "Multiple runtime behavior selected. Select only one! ('kmdf-runtime', 'test-runtime')"
 );
 
-#[cfg(all(feature = "test-runtime", feature = "kmdf-runtime"))]
+#[cfg(all(
+    feature = "test-runtime",
+    feature = "kmdf-runtime"
+))]
 compile_error!(
     "Select a valid runtime behavior for swdk. ('kmdf-runtime', 'test-runtime')"
 );
-
 pub extern crate alloc;
 
 mod runtime;
@@ -29,16 +34,14 @@ pub mod rt {
     #[cfg(feature = "kmdf-runtime")]
     mod __kmdf_rt {
         pub use wdk;
-        pub use wdk_sys;
         pub use wdk_alloc;
+        pub use wdk_sys;
 
         pub extern crate wdk_panic;
 
-        pub use crate::runtime::logging;
-        pub use crate::runtime::utils;
-        
         /// KMDFS internal callbacks
         pub use crate::runtime::kmdf as __cb;
+        pub use crate::runtime::{logging, utils};
     }
 
     #[cfg(feature = "kmdf-runtime")]
@@ -50,26 +53,24 @@ pub mod rt {
 
 #[doc(hidden)]
 pub use paste::paste as __swdk_paste;
-
 #[cfg(feature = "kmdf-runtime")]
 pub use wdk::println;
 
 mod __public_api {
-    pub use crate::wdf::handle::*;
-    pub use crate::wdf::ioctl;
-
-    /// Contains various WDF convertible vals
-    pub use crate::wdf::values as vals;
-
-    /// Contains WDF context types
-    pub use crate::wdf::context as ctx;
-
     /// Contains WDF builders
     pub use crate::wdf::builders as bd;
 
+    /// Contains WDF context types
+    pub use crate::wdf::context as ctx;
+    pub use crate::wdf::handle::*;
+    pub use crate::wdf::ioctl;
+
     /// Contains WDF operators
     pub use crate::wdf::operators as op;
+    /// Contains various WDF convertible vals
+    pub use crate::wdf::values as vals;
 }
 
 pub use __public_api::*;
+
 pub use crate::wdf::generators as gens;
