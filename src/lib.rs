3e6ab648c3d129771a4cba9e5755384c2cf263eb
Spartaco Amadei <spartaco.amadei@outlook.it>
2026-08-02 05:44:37 +0200

    feat: expose new `HandleMut` for mutable kernel resources

--- src/lib.rs
+++ src/lib.rs
@@ -62,7 +62,7 @@ mod __public_api {
 
     /// Contains WDF context types
     pub use crate::wdf::context as ctx;
-    pub use crate::wdf::handle::Handle;
+    pub use crate::wdf::handle::{Handle, HandleMut};
     pub use crate::wdf::ioctl;
 
     /// Contains WDF operators
